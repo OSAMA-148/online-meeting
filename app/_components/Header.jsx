d9d6381b3545ccd418f228320331062f15c8c6b4
osama <om2951855@gmail.com>
2025-07-25 21:23:04 +0300

    finish header component

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    return (
+        <div>
+            <div className="flex justify-between items-center p-4">
+                <Image
+                    className="w-[70px] md:w-[100px]"
+                    width={70}
+                    height={70}
+                    alt="logo"
+                    src="/logo.png"
+                />
+                <ul className=" hidden text-[#039C96] font-bold  md:flex gap-14">
+                    <li className="hover:bg-[#d0efee] transition-all duration-300 p-2 cursor-pointer">
+                        Product
+                    </li>
+                    <li className="hover:bg-[#d0efee] transition-all duration-300 p-2 cursor-pointer">
+                        Pricing
+                    </li>
+                    <li className="hover:bg-[#d0efee] transition-all duration-300 p-2 cursor-pointer">
+                        About Us
+                    </li>
+                    <li className="hover:bg-[#d0efee] transition-all duration-300 p-2 cursor-pointer">
+                        Contact Us
+                    </li>
+                </ul>
+                <div className="">
+                    <Button className="bg-[#039C96] mr-3 cursor-pointer">
+                        Login
+                    </Button>
+                    <Button className="bg-[#FEBF2F] cursor-pointer">
+                        Get Started
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Header
+export default Header;
