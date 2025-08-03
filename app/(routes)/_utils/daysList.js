8f706357f635a414bb10be4c53e1a6967b581d5c
osama <om2951855@gmail.com>
2025-08-03 18:52:24 +0300

    add utils

---
+++ app/(routes)/_utils/daysList.js
@@ -0,0 +1,27 @@
+const days = [
+    {
+        day: "sunday",
+    },
+
+    {
+        day: "monday",
+    },
+
+    {
+        day: "tuesday",
+    },
+
+    {
+        day: "wednesday",
+    },
+
+    {
+        day: "thursday",
+    },
+
+    {
+        day: "friday",
+    },
+];
+
+export default days;
---
+++ app/(routes)/_utils/program.js
@@ -0,0 +1,18 @@
+const porgrams = [
+    {
+        name: "meet",
+        icon: "/meet.png",
+    },
+
+    {
+        name: "phone",
+        icon: "/app.png",
+    },
+
+    {
+        name: "zoom",
+        icon: "/business.png",
+    },
+];
+
+export default porgrams;
